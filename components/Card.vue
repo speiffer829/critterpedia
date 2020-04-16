70c99423f1f1464fff135e016fa3e0f9b572425c
Spenser <spenser@spenserpeiffer.com>
2020-04-15 20:07:27 -0400

    pics

---
+++ components/Card.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="card">
+    <div class="img-bar" :style="{backgroundColor: imgColor}">
+      <div class="img-align">
+        <div class="img-circle" :style="{backgroundColor: imgColor}">
+          <img :src="`/fish/${fish.img}.png`" alt="fish">
+        </div>
+      </div>
+    </div>
+    <p class="title">{{ fish.title }}</p>
+    
+    <div class="months">
+      <p class="head">Months</p>
+      <span :class="{'active' : jan}">Jan</span>
+      <span :class="{'active' : feb}">Feb</span>
+      <span :class="{'active' : mar}">Mar</span>
+      <span :class="{'active' : apr}">Apr</span>
+      <span :class="{'active' : may}">May</span>
+      <span :class="{'active' : jun}">Jun</span>
+      <span :class="{'active' : jul}">Jul</span>
+      <span :class="{'active' : aug}">Aug</span>
+      <span :class="{'active' : sept}">Sept</span>
+      <span :class="{'active' : oct}">Oct</span>
+      <span :class="{'active' : nov}">Nov</span>
+      <span :class="{'active' : dec}">Dec</span>
+    </div>
+
+    <div class="times">
+      <div class="time-grid">
+        <p class="head">Time Active</p>
+        <span :class="{ 'active' : fish.times.includes(1) }">1 am</span>
+        <span :class="{ 'active' : fish.times.includes(13) }">1 pm</span>
+        <span :class="{ 'active' : fish.times.includes(2) }">2 am</span>
+        <span :class="{ 'active' : fish.times.includes(14) }">2 pm</span>
+        <span :class="{ 'active' : fish.times.includes(3) }">3 am</span>
+        <span :class="{ 'active' : fish.times.includes(15) }">3 pm</span>
+        <span :class="{ 'active' : fish.times.includes(4) }">4 am</span>
+        <span :class="{ 'active' : fish.times.includes(16) }">4 pm</span>
+        <span :class="{ 'active' : fish.times.includes(5) }">5 am</span>
+        <span :class="{ 'active' : fish.times.includes(17) }">5 pm</span>
+        <span :class="{ 'active' : fish.times.includes(6) }">6 am</span>
+        <span :class="{ 'active' : fish.times.includes(18) }">6 pm</span>
+        <span :class="{ 'active' : fish.times.includes(7) }">7 am</span>
+        <span :class="{ 'active' : fish.times.includes(19) }">7 pm</span>
+        <span :class="{ 'active' : fish.times.includes(8) }">8 am</span>
+        <span :class="{ 'active' : fish.times.includes(20) }">8 pm</span>
+        <span :class="{ 'active' : fish.times.includes(9) }">9 am</span>
+        <span :class="{ 'active' : fish.times.includes(21) }">9 pm</span>
+        <span :class="{ 'active' : fish.times.includes(10) }">10 am</span>
+        <span :class="{ 'active' : fish.times.includes(22) }">10 pm</span>
+        <span :class="{ 'active' : fish.times.includes(11) }">11 am</span>
+        <span :class="{ 'active' : fish.times.includes(23) }">11 pm</span>
+        <span :class="{ 'active' : fish.times.includes(12) }">12 pm</span>
+        <span :class="{ 'active' : fish.times.includes(0) }">12 am</span>
+      </div>
+    </div>
+
+    <div class="location">
+      <p class="head">Location</p>
+      <p class="stat">{{ fish.location }}</p>
+    </div>
+
+    <div class="size">
+      <p class="head">Size</p>
+      <p class="stat">{{ fish.size }}</p>
+    </div>
+
+    <div class="price" :style="{backgroundColor: imgColor}">
+      <!-- <p class="head">Price</p> -->
+      <p class="stat">${{ prettNum(fish.price) }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Card',
+  props:{
+    fish: Object
+  },
+  data() {
+    return {
+      imgColor: '',
+      jan: false,
+      feb: false,
+      mar: false,
+      apr: false,
+      may: false,
+      jun: false,
+      jul: false,
+      aug: false,
+      sept: false,
+      oct: false,
+      nov: false,
+      dec: false,
+      img: `/fish/${this.fish.img}.png`,
+      publicPath: process.env.BASE_URL,
+    }
+  },
+  mounted() {
+    const price = this.fish.price;
+    if(price >= 10000){
+      this.imgColor = '#FCCC1F';
+    }else if(price >= 5000){
+      this.imgColor = '#B6A2C8';
+    }else if(price >= 1000){
+      this.imgColor = '#74A1D2';
+    }else{
+      this.imgColor = '#D1DB63';
+    }
+
+    this.fish.months.forEach(month => {
+      this[month] = true;
+    });
+  },
+  methods: {
+    prettNum(num){
+      var num_parts = num.toString().split(".");
+      num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      return num_parts.join(".");
+    }
+  },
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -4,52 +4,306 @@
   </div>
 </template>
 
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+<style lang="scss">
+@import '@/assets/global.scss';
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
+body{
+  background-color: $green;
+  background-image: url('~@/assets/green-triangles.png');
+  background-size: 156px 154.5px;
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+#app {
+  font-family: $font;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+h1{
+  text-align: center;
+  font-size: 4rem;
+  color: $gold;
+  font-family: $headFont;
+  text-shadow: 2px 2px 5px darken($green, 15%);
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+#nav {
+  padding: 30px;
+  display: grid;
+  justify-content: center;
+  max-width: 330px;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-content: center;
+  margin: 0 auto;
+  grid-gap: 0.8rem;
+
+
+  a {
+    font-weight: bold;
+    background-color: $brown;
+    color: $gold;
+    text-shadow: 1px 1px 3px darken($brown, 10%);
+    background-image: url('~@/assets/play-dots.png');
+    background-size: 36px 34.5px;
+    padding: 0.4rem;
+    text-decoration: none;
+    font-family: $headFont;
+    border-radius: 0.2rem;
+    font-size: 1.3rem;
+    text-align: center;
+    box-shadow: 0px 2px 5px darken($green, 15%);
+    transition: all 250ms;
+
+    &:hover{
+      transform: scale(1.15);
+    }
+
+    &.router-link-exact-active {
+      background-color: $gold;
+      color: $brown;
+      text-shadow: 1px 1px 3px darken($gold, 15%);
+    }
+  }
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.animal-grid{
+  display: grid;
+  grid-gap: 2rem;
+  margin: 10px auto;
+  max-width: 1200px;
+  width: 95%;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  justify-content: center;
+  padding-bottom: 2rem;
+
+  .card{
+    background-color: $tan;
+    background-image: url('~@/assets/play-dots.png');
+    background-size: 36px 34.5px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-areas: 
+      'pic pic pic'
+      'title title title'
+      'price price price'
+      'size location location'
+      'month time time';
+    border-radius: 0.5rem;
+    filter: drop-shadow(2px 2px 10px darken($green,10%));
+    margin-top: 40px;
+    text-align: center;
+
+    .img-bar{
+      height: 10px;
+      border-radius: 0.5rem 0.5rem 0 0;
+      grid-area: pic;
+      position: relative;
+      background-image: url('~@/assets/play-dots.png');
+      background-size: 36px 34.5px;
+
+      .img-align{
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        top: -40px;
+
+        .img-circle{
+          width: 80px;
+          height: 80px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border-radius: 200px;
+          background-image: url('~@/assets/play-dots.png');
+          background-size: 36px 34.5px;
+
+          img{
+            display: block;
+          }
+        }
+      }
+    }
+
+    .title{
+      font-size: 2rem;
+      color: $gold;
+      font-family: $headFont;
+      padding: 0.8rem;
+      padding-top: 2.2rem;
+      font-weight: bold;
+      background: $brown;
+      margin: 0;
+      text-shadow: 2px 2px 5px darken($brown, 10%);
+      background-image: url('~@/assets/play-dots.png');
+      background-size: 36px 34.5px;
+      grid-area: title;
+    }
+
+    .head{
+      margin: 0 auto;
+      text-align: center;
+      font-family: $headFont;
+      font-weight: bold;
+      display: inline-block;
+      background: $gold;
+      color: darken($brown, 5%);
+      padding: 5px 10px;
+      text-align: center;
+      border-radius: 20px;
+      margin-top: 10px;
+      // text-shadow: 1px 1px 0px darken($brown, 5%);
+    }
+
+    .stat{
+      font-size: 1.2rem;
+      text-transform: capitalize;
+      font-weight: bold;
+      font-family: $headFont;
+      margin: 10px 0;
+    }
+
+    .months{
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-gap: 5px;
+      padding: 10px;
+      color: $brown;
+      grid-area: month;
+
+      span{
+        border-radius: 4px;
+        text-align: center;
+        color: $brown;
+        font-weight: bold;
+        border: solid 2px $brown;
+        display: block;
+        box-sizing: border-box;
+        background: $tan;
+
+        &.active{
+          background: $brown;
+          color: $gold;
+          // background-image: url('~@/assets/play-dots.png');
+          // background-size: 36px 34.5px;
+        }
+      }
+    }
+
+    .times{
+      grid-area: time;
+      // background: radial-gradient(lighten($blue, 15%), $blue);
+      color: $brown;
+      padding: 10px;
+      
+      .time-grid{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 5px;
+
+        .head{
+          grid-column: span 2;
+        }
+      }
+
+      span{
+        border-radius: 4px;
+        text-align: center;
+        color: $brown;
+        font-weight: bold;
+        border: solid 2px $gold;
+        width: 100%;
+        display: block;
+        box-sizing: border-box;
+        background: $tan;
+
+        &.active{
+          background-color: $gold;
+          color: #000;
+        }
+      }
+      
+      p{
+        text-align: center;
+        font-family: $headFont;
+      }
+    }
+
+    .location{
+      grid-area: location;
+      padding: 10px;
+      color: $gold;
+      background-color: darken($brown,15%);
+      background-image: url('~@/assets/play-dots.png');
+      background-size: 36px 34.5px;s
+      
+      p{
+        text-align: center;
+        font-family: $headFont;
+      }
+    }
+
+    .size{
+      grid-area: size;
+      padding: 10px;
+      color: $gold;
+      background-color: darken($brown,10%);
+      background-image: url('~@/assets/play-dots.png');
+      background-size: 36px 34.5px;
+      
+      p{
+        text-align: center;
+        font-family: $headFont;
+      }
+    }
+
+    .price{
+      grid-area: price;
+      padding: 10px;
+      color: $brown;
+      background-color: $gold;
+      background-image: url('~@/assets/play-dots.png');
+      background-size: 36px 34.5px;
+
+      .head{
+        background: none;
+        color: $tan;
+        text-shadow: 1px 1px 0px darken($tan, 70%);
+      }
+
+      .stat{
+        font-size: 2rem;
+        color: $tan;
+        text-shadow: 1px 1px 0px darken($tan, 70%);
+      }
+      
+      p{
+        text-align: center;
+        font-family: $headFont;
+      }
+    }
+  }
+}
+
+
+.search-contain{
+  max-width: 960px;
+  width: 95%;
+  margin: 20px auto;
+
+  input{
+    width: 100%;
+    font-size: 2rem;
+    padding: 0.6rem;
+    font-family: $headFont;
+    background: $tan;
+    color: $brown;
+    border: solid 3px $brown;
+    border-radius: 10px;
+    box-sizing: border-box;
+    display: block;
+  }
 }
 </style>
